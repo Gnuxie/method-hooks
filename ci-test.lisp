250bfacefe4e450884f599ced1d65c28f428d77b
Gnuxie <gnuxie@protonmail.com>
2019-05-22 13:20:10 +0100

    fix ci-run

--- ci-test.lisp
+++ ci-test.lisp
@@ -2,5 +2,4 @@
 
 (ql:register-local-projects)
 (ql:quickload '(:method-hooks-test :uiop))
-(unwind-protect (method-hooks-test:ci-run) ; ci-run should quit before we do
-  (uiop:quit -1))                          ; we have to do this for implementations that don't have --quit or disable debugger
+(method-hooks-test:ci-run) ; ci-run should quit before we do
